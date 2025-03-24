e4cf4bea9c3cdda6ee9c8e88b02c41adb02544a0
e_cjaramillo <e_cjaramillo@bancoppel.com>
2025-03-24 03:33:54 -0600

    first commit

---
+++ src/main/java/mx/bank/ms/currency_exchange/MsCurrencyExchangeApplication.java
@@ -0,0 +1,13 @@
+package mx.bank.ms.currency_exchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCurrencyExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCurrencyExchangeApplication.class, args);
+	}
+
+}
