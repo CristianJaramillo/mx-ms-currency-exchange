b08327907abf6fdf63bcb27d20d4ce3c29a8ec15
e_cjaramillo <e_cjaramillo@bancoppel.com>
2025-03-27 23:54:11 -0600

    Remove unused imports

--- src/main/java/mx/bank/ms/currency_exchange/infrastructure/external/exchangerateapi/ExchangeRateAPIConfiguration.java
+++ src/main/java/mx/bank/ms/currency_exchange/infrastructure/external/exchangerateapi/ExchangeRateAPIConfiguration.java
@@ -1,6 +1,5 @@
 package mx.bank.ms.currency_exchange.infrastructure.external.exchangerateapi;
 
-import feign.Contract;
 import feign.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
