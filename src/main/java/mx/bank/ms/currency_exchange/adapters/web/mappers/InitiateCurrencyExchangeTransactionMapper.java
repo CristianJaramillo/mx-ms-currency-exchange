f5da82ca080af6148ef711104ab5e86d7cc0c8c8
e_cjaramillo <e_cjaramillo@bancoppel.com>
2025-06-25 15:49:01 -0600

    update pom.xml

--- src/main/java/mx/bank/ms/currency_exchange/adapters/web/mappers/InitiateCurrencyExchangeTransactionMapper.java
+++ src/main/java/mx/bank/ms/currency_exchange/adapters/web/mappers/InitiateCurrencyExchangeTransactionMapper.java
@@ -1,6 +1,5 @@
 package mx.bank.ms.currency_exchange.adapters.web.mappers;
 
-import mx.bank.ms.currency_exchange.adapters.web.resources.AmountAndCurrency;
 import mx.bank.ms.currency_exchange.adapters.web.resources.InitiateCurrencyExchangeTransactionRequest;
 import mx.bank.ms.currency_exchange.adapters.web.resources.InitiateCurrencyExchangeTransactionResponse;
 import mx.bank.ms.currency_exchange.domain.model.CurrencyExchangeTransactionModel;
@@ -8,8 +7,6 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
-
-import java.math.BigDecimal;
 
 @Mapper(componentModel = "spring", uses = AmountAndCurrencyMapperHelper.class)
 public interface InitiateCurrencyExchangeTransactionMapper {
